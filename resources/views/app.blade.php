5100bcaa6ce00760d1e42938c66364c6506863e9
Luan2121 <Luan.esr21@gmail.com>
2021-03-06 23:19:37 -0400

    public folder updated

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<html lang=”{{app()->getLocale()}}”>
+    <head>
+        <meta charset=”utf-8">
+        <meta http-equiv=”X-UA-Compatible” content=”IE=edge”>
+        <meta name=”viewport” content=”width=device-width, initial-scale=1">
+        <! — csrf token →
+        <meta name=”csrf-token” content=”{{ csrf_token() }}”>
+
+        <title>App Blade</title>
+        <! — styles →
+    </head>
+    <body>
+        <div id="root"></div>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
